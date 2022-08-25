8f32a905c1d24814adbc057d6755098186f4e2b6
Snowiewie <80443587+Snowiewie@users.noreply.github.com>
2022-08-25 15:31:36 +0800

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -4,13 +4,17 @@ $_SESSION["host"] = "us-cdbr-east-06.cleardb.net"; //provide hostname
 $_SESSION["user"] = "ba1fc85c5d5566"; //user name for your remote server
 $_SESSION["password"] = "2b011857"; //password
 $_SESSION["auth_type"] = "config"; // keep it as
+$sname = "localhost";
+$uname = "root";
+$password = "";
+
 
 
 $db_name = "heroku_dae2d78a90708c9";
 
 
-
-        $conn = new mysqli($_SESSION["host"], $_SESSION["user"], $_SESSION["password"], $_SESSION["auth_type"]);
+        $conn = mysqli_connect($sname, $uname, $password, $db_name);
+        // $conn = new mysqli($_SESSION["host"], $_SESSION["user"], $_SESSION["password"], $_SESSION["auth_type"]);
 
         // Check connection
         if ($conn->connect_error) {
